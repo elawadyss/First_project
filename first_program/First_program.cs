21513c6ec111512a12fb33b64996d2d4c14fe7be
Sameh Elawady <sameh@ELAWADYS>
2017-02-12 17:25:33 -0800

    adding comment to the file

--- first_program/First_program.cs
+++ first_program/First_program.cs
@@ -13,8 +13,8 @@ namespace first_program
             Console.WriteLine("Hello world"); // Write: for writing in the same line ... Writeline: for writing in new line
             Console.Write("hello world 2"); // ALT.CTRL+l for viewing solution bar. // C# is a block sturcture language. there may be nested block. //forgive white space.
             Console.Write("it is good"); Console.WriteLine(":not bad"); // many programs at one line ..// case sensitive.
-            int my_value = 5;
-            int x;
+            int my_value = 5; // integer value
+            int x; // integer variable without value assignment
             Console.ReadKey(); // press any key to terminate the command line .. CTRl+K .then. CTRl+C to make comments. CTRl+K .then. CTRl+u to remove comments.
 
         }
