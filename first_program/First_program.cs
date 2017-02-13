e21891ffd8e47801e7f77eb44a1adf8aa7dfc5da
sameh <sameh@ELAWADYS>
2017-02-12 17:16:08 -0800

    Add project files.

---
+++ first_program/First_program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_program 
+{
+    class First_program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world"); // Write: for writing in the same line ... Writeline: for writing in new line
+            Console.Write("hello world 2"); // ALT.CTRL+l for viewing solution bar. // C# is a block sturcture language. there may be nested block. //forgive white space.
+            Console.Write("it is good"); Console.WriteLine(":not bad"); // many programs at one line ..// case sensitive.
+            int my_value = 5;
+            int x;
+            Console.ReadKey(); // press any key to terminate the command line .. CTRl+K .then. CTRl+C to make comments. CTRl+K .then. CTRl+u to remove comments.
+
+        }
+    }
+}
